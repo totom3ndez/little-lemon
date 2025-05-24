a5ff7cd4fcf29617e1546105b88157a7b209c410
totom3ndez <tomii.mendez45@gmail.com>
2025-05-24 16:20:00 +0200

    Refactor styles and structure for Little Lemon website; remove unused index.css, enhance App component layout, and define CSS variables for better theming.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import './App.css'
+import Header from './components/Header'
+import Main from './components/Main'
+import Footer from './components/Footer'
 
 function App() {
-  return <h1>Little lemon</h1>
+  return (
+    <>
+    <Header />
+    <Main />
+    <Footer />
+    </>
+  )
 }
 
 export default App
