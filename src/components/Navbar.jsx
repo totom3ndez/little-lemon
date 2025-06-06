27f7bd4d3645e0ea92435328488e45d1cf647955
totom3ndez <tomii.mendez45@gmail.com>
2025-06-07 00:29:59 +0200

    Enhance Navbar component for mobile responsiveness: implement dynamic menu toggle and adjust layout for smaller screens. Update styles for mobile navigation.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,67 @@
+import { useEffect } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom"
 
 const Navbar = () => {
+
+  const [isMobile, setIsMobile] = useState(false);
+  const [isOpen, setIsOpen] = useState(false)
+  const handleResize = () => {
+    setIsMobile(window.innerWidth < 768);
+  }
+
+  useEffect(() => {
+  handleResize(); // Set initial state
+  window.addEventListener('resize', handleResize);
+  return () => {
+    window.removeEventListener('resize', handleResize);
+  };
+}, []);
+
+const handleMenu = () => {
+  setIsOpen(prev => !prev)
+}
+
+
   return (
-    <ul className="navbar">
-      <li><a href="/">Home</a></li>
-      <li><a href="#about">About</a></li>
-      <li><Link to="/menu">Menu</Link></li>
-      <li><Link to="/booking">Reservations</Link></li>
-      <li><Link to="/order-online">Order Online</Link></li>
-      <li><Link to="/login">Login</Link></li>
-    </ul>
+    <>
+    {
+      isMobile
+      ?
+      <>
+        {
+          isOpen
+          ?
+          <ul className="navbar-mobile">
+            <button onClick={handleMenu} className="closer">
+              <img src="./svg/close.svg" alt="closer" />
+            </button>
+            <li><a onClick={handleMenu} href="/">Home</a></li>
+            <li><a onClick={handleMenu} href="#about">About</a></li>
+            <li><Link onClick={handleMenu} to="/menu">Menu</Link></li>
+            <li><Link onClick={handleMenu} to="/booking">Reservations</Link></li>
+            <li><Link onClick={handleMenu} to="/order-online">Order Online</Link></li>
+            <li><Link onClick={handleMenu} to="/login">Login</Link></li>
+          </ul>
+          :
+          <button onClick={handleMenu} className="opener-button">
+            <img src="./svg/hamburger.svg" alt="opener" />
+          </button>
+        }
+      
+      </>
+    :
+      <ul className="navbar">
+        <li><a href="/">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><Link to="/menu">Menu</Link></li>
+        <li><Link to="/booking">Reservations</Link></li>
+        <li><Link to="/order-online">Order Online</Link></li>
+        <li><Link to="/login">Login</Link></li>
+      </ul>
+    
+  }
+  </>
   )
 }
 
